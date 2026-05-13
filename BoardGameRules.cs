3ca03e3059a32466ac19c6de46ee2361699cd554
d1mw1t <crydmwt@gmail.com>
2026-05-13 22:24:30 +1000

    Added BoardGameRules class and LineBasedGameRules class

---
+++ BoardGameRules.cs
@@ -0,0 +1,20 @@
+namespace OOP_BoardGames_Framework
+{
+    public abstract class BoardGameRules : GameRules
+    {
+        public abstract int Columns { get; }
+        public abstract int Rows { get; }
+        public abstract int WinLength { get; }
+        public abstract bool ValidatePlayerMove(Board board, PlayerMove move);
+        public abstract bool CheckForWinning(Board board);
+        public abstract void ExecuteMove(Board board, PlayerMove move, string playerSymbol);
+        protected bool IsInsideBoard(int column, int row)
+        {
+            return column >= 0 && column < Columns && row >= 0 && row < Rows;
+        }
+        public virtual bool CheckForDraw(Board board)
+        {
+            return board.IsBoardFull();
+        }
+    }
+}
