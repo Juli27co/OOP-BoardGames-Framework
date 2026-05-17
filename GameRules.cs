09e8bda584f4344ffafd039e4148a91a0e01c164
d1mw1t <crydmwt@gmail.com>
2026-05-17 20:15:06 +1000

    Minor GameRules change

--- GameRules.cs
+++ GameRules.cs
@@ -11,12 +11,11 @@ namespace OOP_BoardGames_Framework
         public abstract void ExecuteMove(Board board, PlayerMove move, string playerSymbol);
         protected bool IsInsideBoard(int column, int row)
         {
-            return column >= 0 && column < Columns && row >= 0 & row < Rows;
+            return column >= 0 && column < Columns && row >= 0 && row < Rows;
         }
         public virtual bool CheckForDraw(Board board)
         {
             return board.IsBoardFull();
-            //return false;
         }
     }
 }
