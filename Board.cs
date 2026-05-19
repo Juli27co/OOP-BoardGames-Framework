3baf9865bf18048c1826d420fe535a4201370079
Jenna <heo980201@gmail.com>
2026-05-19 22:54:11 +1000

    added Clear()

--- Board.cs
+++ Board.cs
@@ -85,5 +85,16 @@
                 column >= 0 &&
                 column < GetColumnCount();
         }
+
+        public void Clear()
+        {
+            for (int row = 0; row < GetRowCount(); row++)
+            {
+                for (int column = 0; column < GetColumnCount(); column++)
+                {
+                    gridMatrix[row][column] = " ";
+                }
+            }
+        }
     }
 }
