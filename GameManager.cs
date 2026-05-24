63b17c2d384350d47cc4b7ba465c8d4d02febcee
Jenna <heo980201@gmail.com>
2026-05-24 13:01:43 +1000

    Add colour coded display for Connect Four

--- GameManager.cs
+++ GameManager.cs
@@ -124,7 +124,7 @@
             BoardDisplay boardDisplay = new BoardDisplay();
             if (Type == GameType.ConnectFour)
             {
-                boardDisplay.ShowConnectFourBoard(board!);
+                boardDisplay.ShowConnectFourBoard(board!, rules as ConnectFourRules);
             }
             else if (Type == GameType.Gomoku)
             {
